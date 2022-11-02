52027f9d78bfc79825353bde23475202d076c190
nicor88 <6278547+nicor88@users.noreply.github.com>
2022-11-02 12:15:29 +0100

    change indent

--- dbt/include/glue/macros/materializations/table/iceberg_table_replace.sql
+++ dbt/include/glue/macros/materializations/table/iceberg_table_replace.sql
@@ -1,24 +1,23 @@
 {% materialization iceberg_table_replace, adapter='glue' -%}
-	{%- set default_catalog = 'iceberg_catalog' -%}
-	{%- set partition_by = config.get('partition_by', none) -%}
-	{% set target_relation = this %}
+  {%- set default_catalog = 'iceberg_catalog' -%}
+  {%- set partition_by = config.get('partition_by', none) -%}
+  {% set target_relation = this %}
+  {% set build_sql = create_or_replace(default_catalog, target_relation) %}
 
-	{% set build_sql = create_or_replace(default_catalog, target_relation) %}
+  {%- call statement('main') -%}
+    {{ build_sql }}
+  {%- endcall -%}
 
-	{%- call statement('main') -%}
-     {{ build_sql }}
-  	{%- endcall -%}
-
-  	{{ return({'relations': [target_relation]}) }}
+	{{ return({'relations': [target_relation]}) }}
 
 {%- endmaterialization %}
 
 {% macro create_or_replace(default_catalog, relation) %}
-    create or replace table {{ default_catalog }}.{{ relation }}
-    using ICEBERG
-	{{ partition_cols(label="partitioned by") }}
-	{{ glue__location_clause(relation) }}
-	{{ comment_clause() }}
-	as
-	{{ sql }}
+  create or replace table {{ default_catalog }}.{{ relation }}
+  using ICEBERG
+  {{ partition_cols(label="partitioned by") }}
+  {{ glue__location_clause(relation) }}
+  {{ comment_clause() }}
+  as
+  {{ sql }}
 {% endmacro %}
