220f6e5075d0aeead51157cfe83cf917ad602af3
Mark Olliver <mark.olliver@projectolympus.co.za>
2022-11-30 09:07:24 +0000

    Increase retry and backoff settings for Boto

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
 package_name = "dbt-glue"
-package_version = "0.3.6"
+package_version = "0.3.7"
 dbt_version = "1.3.0"
 description = """dbt (data build tool) adapter for Aws Glue"""
 setup(
