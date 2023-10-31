dede96b786907e1c30e15e7376e1381dc4cbe941
hash0b <149504076+hash0b@users.noreply.github.com>
2023-10-31 18:35:47 +0000

    Pick only valid columns from describe relation output
    Output may contain other records with descriptive text or empty strings

--- dbt/adapters/glue/impl.py
+++ dbt/adapters/glue/impl.py
@@ -242,10 +242,14 @@ class GlueAdapter(SQLAdapter):
             records = self.fetch_all_response(response)
 
             for record in records:
-                column = self.Column(column=record[0], dtype=record[1])
-                if record[0][:1] != "#":
-                    if column not in columns:
-                        columns.append(column)
+                column_name: str = record[0].strip()
+                column_type: str = record[1].strip()
+                if (
+                    column_name.lower() not in ["", "not partitioned"]
+                    and not column_name.startswith('#')
+                ):
+                    column = self.Column(column=column_name, dtype=column_type)
+                    columns.append(column)
 
         except DbtDatabaseError as e:
             raise DbtDatabaseError(msg="GlueGetColumnsInRelationFailed") from e
@@ -521,7 +525,7 @@ if (spark.sql("show tables in {model["schema"]}").where("tableName == '{model["n
     df.write\
         .mode("{session.credentials.seed_mode}")\
         .format("{session.credentials.seed_format}")\
-        .insertInto(table_name, overwrite={mode})   
+        .insertInto(table_name, overwrite={mode})
 else:
     df.write\
         .option("path", "{session.credentials.location}/{model["schema"]}/{model["name"]}")\
@@ -589,7 +593,7 @@ SqlWrapper2.execute("""select * from {model["schema"]}.{model["name"]} limit 1""
 
         if {session.credentials.delta_athena_prefix} is not None:
             run_msck_repair = f'''
-            spark.sql("MSCK REPAIR TABLE {target_relation.schema}.headertoberepalced_{target_relation.name}") 
+            spark.sql("MSCK REPAIR TABLE {target_relation.schema}.headertoberepalced_{target_relation.name}")
             SqlWrapper2.execute("""select 1""")
             '''
             generate_symlink = f'''
@@ -597,7 +601,7 @@ SqlWrapper2.execute("""select * from {model["schema"]}.{model["name"]} limit 1""
             from delta.tables import DeltaTable
             deltaTable = DeltaTable.forPath(spark, "{location}")
             deltaTable.generate("symlink_format_manifest")
-            
+
             '''
             if partition_by is not None:
                 update_manifest_code = generate_symlink + run_msck_repair
@@ -646,7 +650,7 @@ deltaTable = DeltaTable.forPath(spark, "{location}")
 deltaTable.generate("symlink_format_manifest")
 schema = deltaTable.toDF().schema
 columns = (','.join([field.simpleString() for field in schema])).replace(':', ' ')
-ddl = """CREATE EXTERNAL TABLE {target_relation.schema}.headertoberepalced_{target_relation.name} (""" + columns + """) 
+ddl = """CREATE EXTERNAL TABLE {target_relation.schema}.headertoberepalced_{target_relation.name} (""" + columns + """)
 '''
 
         create_athena_table_footer = f'''
@@ -663,7 +667,7 @@ spark.sql(ddl)
 PARTITIONED BY ({part_list})
             '''
             run_msck_repair = f'''
-spark.sql("MSCK REPAIR TABLE {target_relation.schema}.headertoberepalced_{target_relation.name}") 
+spark.sql("MSCK REPAIR TABLE {target_relation.schema}.headertoberepalced_{target_relation.name}")
 SqlWrapper2.execute("""select 1""")
             '''
             write_data_code = write_data_header + write_data_partition + write_data_footer
