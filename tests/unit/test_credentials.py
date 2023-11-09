71309d25441a74fa23f692218ec4567a348cf178
Noritaka Sekiyama <sekiyama@amazon.com>
2023-11-09 14:13:50 +0900

    Added test for Glue default version

--- tests/unit/test_credentials.py
+++ tests/unit/test_credentials.py
@@ -1,14 +1,18 @@
+import unittest
+
 from dbt.adapters.glue.connections import GlueCredentials
 
 
-def test_credentials_database_schema() -> None:
-    credentials = GlueCredentials(
-        database="tests",
-        schema="tests",
-        role_arn="arn:aws:iam::123456789101:role/GlueInteractiveSessionRole",
-        region="ap-northeast-1",
-        workers=4,
-        worker_type="G.2X",
-    )
-    assert credentials.schema == "tests"
-    assert credentials.database is None
+class TestGlueRelation(unittest.TestCase):
+    def test_credentials(self) -> None:
+        credentials = GlueCredentials(
+            database="tests",
+            schema="tests",
+            role_arn="arn:aws:iam::123456789101:role/GlueInteractiveSessionRole",
+            region="ap-northeast-1",
+            workers=4,
+            worker_type="G.2X",
+        )
+        assert credentials.schema == "tests"
+        assert credentials.database is None
+        assert credentials.glue_version == "4.0"    # default Glue version is 4.0
