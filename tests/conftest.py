d8a3f1154223e83bf5e560af014fe793ef02294d
Akira Ajisaka <akiraaj@amazon.com>
2023-11-15 16:29:37 +0900

    More refactor

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,10 +13,17 @@ schema_name = f"dbt_functional_test_{database_suffix}"
 # Note: fixtures with session scope need to be local
 pytest_plugins = ["dbt.tests.fixtures.project"]
 
+# Use different datatabase for each test class
+@pytest.fixture(scope="class")
+def unique_schema(request, prefix) -> str:
+    database_suffix = ''.join(random.choices(string.digits, k=4))
+    return f"dbt_functional_test_{database_suffix}"
+
+
 # The profile dictionary, used to write out profiles.yml
 # dbt will supply a unique schema per test, so we do not specify 'schema' here
 @pytest.fixture(scope="class")
-def dbt_profile_target():
+def dbt_profile_target(unique_schema):
     return {
         'type': 'glue',
         'query-comment': 'test-glue-adapter',
@@ -25,19 +32,14 @@ def dbt_profile_target():
         'region': os.getenv("DBT_GLUE_REGION", 'eu-west-1'),
         'workers': 2,
         'worker_type': 'G.1X',
-        'schema': 'dbt_functional_test_01',
-        'database': 'dbt_functional_test_01',
+        'schema': unique_schema,
+        'database': unique_schema,
         'session_provisioning_timeout_in_seconds': 300,
         'location': os.getenv('DBT_S3_LOCATION'),
         'datalake_formats': 'delta',
         'conf': "spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog --conf spark.sql.legacy.allowNonEmptyLocationInCTAS=true",
         'glue_session_reuse': True
     }
-
-
-@pytest.fixture(scope="class")
-def unique_schema(request, prefix) -> str:
-    return schema_name
 
 
 @pytest.fixture(scope="class")
@@ -49,6 +51,6 @@ def profiles_config_update(dbt_profile_target, unique_schema):
 
 
 @pytest.fixture(scope='class', autouse=True)
-def cleanup():
-    cleanup_s3_location(s3bucket + schema_name, region)
+def cleanup(unique_schema):
+    cleanup_s3_location(s3bucket + unique_schema, region)
     yield
