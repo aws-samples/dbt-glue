28fdcf7563cc28f7d089ab426c160c24f5305bb1
Akira Ajisaka <akiraaj@amazon.com>
2024-01-24 15:56:47 +0900

    Revert several commits to fix the unit and integration tests (#319)
    * Revert "Bump black from 23.11.0 to 23.12.0 (#303)"
    
    This reverts commit ee2aa88663fd51ffc1ad1a1dbe04321206453386.
    
    * Revert "Update versions"
    
    This reverts commit f6a377e9f67ef26ca29ae4138638cd5b7062dad4.
    
    * Revert "Adds minimal model contract enforcement for glue adapter (#297)"
    
    This reverts commit 419234625aa377e9f16d2ff40fb2aeb9c92ad83f.
    
    * Revert "Fix wrong role create db#285 (#286)"
    
    This reverts commit 1416de86faea5b61ebd2b5ac16f862d8ec39f32e.
    
    * Revert "Glue session: enable users to fix their glue_session_id name and re-use it (#301)"
    
    This reverts commit d2d31f109c292efdeba2c1b588cd1cbc60f32a71.
    
    * Revert "Revert "Update versions""
    
    This reverts commit 2b5e65614ebc1e7e6d44ac1a1fe458d299cc0f25.
    
    * Revert "Revert "Bump black from 23.11.0 to 23.12.0 (#303)""
    
    This reverts commit 9a2b09419c3dea549c8a58c569623b0b2e4bae90.
    
    * Update CHANGELOG

--- dbt/adapters/glue/impl.py
+++ dbt/adapters/glue/impl.py
@@ -11,9 +11,9 @@ from concurrent.futures import Future
 
 from dbt.adapters.base import available
 from dbt.adapters.base.relation import BaseRelation
+from dbt.adapters.base.column import Column
 from dbt.adapters.sql import SQLAdapter
 from dbt.adapters.glue import GlueConnectionManager
-from dbt.adapters.glue.column import GlueColumn
 from dbt.adapters.glue.gluedbapi import GlueConnection
 from dbt.adapters.glue.relation import SparkRelation
 from dbt.adapters.glue.lakeformation import (
@@ -33,7 +33,6 @@ logger = AdapterLogger("Glue")
 class GlueAdapter(SQLAdapter):
     ConnectionManager = GlueConnectionManager
     Relation = SparkRelation
-    Column = GlueColumn
 
     relation_type_map = {'EXTERNAL_TABLE': 'table',
                          'MANAGED_TABLE': 'table',
@@ -87,15 +86,15 @@ class GlueAdapter(SQLAdapter):
                     RoleSessionName="dbt"
                 )
                 credentials = assumed_role_object['Credentials']
-                glue_client = boto3.client("glue", region_name=glueSession.credentials.region,
-                                    aws_access_key_id=credentials['AccessKeyId'],
-                                    aws_secret_access_key=credentials['SecretAccessKey'],
-                                    aws_session_token=credentials['SessionToken'])
-                return glueSession, glue_client
+                session = boto3.Session(
+                    aws_access_key_id=credentials['AccessKeyId'],
+                    aws_secret_access_key=credentials['SecretAccessKey'],
+                    aws_session_token=credentials['SessionToken']
+                )
 
-        glue_client = boto3.client("glue", region_name=glueSession.credentials.region)
+        client = boto3.client("glue", region_name=glueSession.credentials.region)
 
-        return glueSession, glue_client
+        return glueSession, client
 
     def list_schemas(self, database: str) -> List[str]:
         session, client = self.get_connection()
