fed67145b66e0d6033baf75de7c4c5125e4c884f
Vincent.PAUWELS <vincent.pauwels@airliquide.com>
2024-09-12 16:50:13 +0200

    fix: replace raise error

--- dbt/adapters/glue/gluedbapi/connection.py
+++ dbt/adapters/glue/gluedbapi/connection.py
@@ -143,11 +143,11 @@ class GlueConnection:
         except WaiterError as we:
             # If it comes here, creation failed, do not re-try (loop)
             logger.exception(f'Connect failed to setup a session for {self.session_id}')
-            raise dbterrors.FailedToConnectError(str(we))
+            raise FailedToConnectError(str(we))
         except Exception as e:
             # If it comes here, creation failed, do not re-try (loop)
             logger.exception(f'Error during connect for session {self.session_id}')
-            raise dbterrors.FailedToConnectError(str(e))
+            raise FailedToConnectError(str(e))
 
     def _create_session(
         self,
@@ -228,7 +228,7 @@ class GlueConnection:
             statement.execute()
         except Exception as e:
             logger.error(f"Error in GlueCursor (session_id={self.session_id}, SQLPROXY) execute: {e}")
-            raise dbterrors.ExecutableError(str(e))
+            raise ExecutableError(str(e))
 
         statement = GlueStatement(client=self.client, session_id=self.session_id,
                                   code=f"spark.sql('use {self.credentials.database}')")
@@ -237,7 +237,7 @@ class GlueConnection:
             statement.execute()
         except Exception as e:
             logger.error(f"Error in GlueCursor (session_id={self.session_id}, SQLPROXY) execute: {e}")
-            raise dbterrors.ExecutableError(str(e))
+            raise ExecutableError(str(e))
 
     @property
     def session_id(self):
