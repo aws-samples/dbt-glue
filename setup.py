a4c31312971e08676964594de39dbbddc5a10b78
Noritaka Sekiyama <sekiyama@amazon.com>
2024-09-25 17:35:34 +0900

    Preparing for Release v1.8.6

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_version(rel_path):
 
 package_name = "dbt-glue"
 package_version = get_version("dbt/adapters/glue/__version__.py")
-dbt_version = "1.8.1"
+dbt_version = "1.8.6"
 dbt_spark_version = "1.8.0"
 description = """dbt (data build tool) adapter for Aws Glue"""
 long_description = read('README.md')
