27a777d7318dd5e1a948c60c58c902cc9ddb6cf4
Noritaka Sekiyama <moomindani@gmail.com>
2025-04-14 19:05:09 +0900

    Preparing for v1.9.4 (#545)

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ def get_version(rel_path):
 
 package_name = "dbt-glue"
 package_version = get_version("dbt/adapters/glue/__version__.py")
-dbt_version = "1.9.2"
-dbt_spark_version = "1.9.1"
+dbt_version = "1.9.4"
+dbt_spark_version = "1.9.2"
 description = """dbt adapter for AWS Glue"""
 long_description = read('README.md')
 setup(
