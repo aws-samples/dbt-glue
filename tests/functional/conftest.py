b4099f1d98c2cc2bb8f01fc964d61c07d38d2158
3tCAknmNMBaxhJ <nathan.a.howard@protonmail.com>
2025-07-14 13:53:14 -0500

    added ability to provide database/schema for functional integration tests instead of unique if needed

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -43,7 +43,7 @@ def dbt_profile_target(unique_schema, use_arrow):
         'location': get_s3_location(),
         'datalake_formats': 'delta,iceberg',
         'conf': f"spark.sql.extensions=io.delta.sql.DeltaSparkSessionExtension,org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.delta.catalog.DeltaCatalog --conf spark.sql.legacy.allowNonEmptyLocationInCTAS=true --conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog --conf spark.sql.catalog.glue_catalog.warehouse={get_s3_location()} --conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog --conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO --conf spark.sql.sources.partitionOverwriteMode=dynamic",
-        'glue_session_reuse': False,
+        'glue_session_reuse': True,
         'use_arrow': use_arrow
     }
 
