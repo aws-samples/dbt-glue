0908f3e754cbb0a0a0880893342ab064a2f4c84d
Noritaka Sekiyama <sekiyama@amazon.com>
2025-10-09 09:15:42 +0530

    Upgrade dbt-core to 1.10.13 along with a few dependencies

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_version(rel_path):
 
 package_name = "dbt-glue"
 package_version = get_version("dbt/adapters/glue/__version__.py")
-dbt_version = "1.10.11"
+dbt_version = "1.10.13"
 dbt_spark_version = "1.9.3"
 description = """dbt adapter for AWS Glue"""
 long_description = read('README.md')
